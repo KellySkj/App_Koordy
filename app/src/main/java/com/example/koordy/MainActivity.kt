1efdd8cf539a70272387821702644eecb891d10e
Aurélie <nass.aurelie@gmail.com>
2026-03-12 12:02:58 +0100

    Prise en compte de l'appareil photo

--- app/src/main/java/com/example/koordy/MainActivity.kt
+++ app/src/main/java/com/example/koordy/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.koordy
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Active le mode bord à bord
         enableEdgeToEdge()
+        
         setContentView(R.layout.home_page)
+
+        // On applique le padding système au bandeau d'annonce pour ne pas être sous l'encoche
+        val banner = findViewById<android.view.View>(R.id.announcement_banner)
+        ViewCompat.setOnApplyWindowInsetsListener(banner) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(v.paddingLeft, systemBars.top, v.paddingRight, v.paddingBottom)
+            insets
+        }
     }
 }
